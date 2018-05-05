e620b47685169aa9bcc384623fb8c49fcf34cb3e
alabaojo <stevxl@gmail.com>
2018-05-05 10:22:20 +0200

    dataJpa ok /authors /books working

--- src/main/java/com/springprojects/spring5webapp/model/Author.java
+++ src/main/java/com/springprojects/spring5webapp/model/Author.java
@@ -17,6 +17,10 @@ public class Author {
     @ManyToMany(mappedBy = "authors")
     private Set<Book> books = new HashSet<>();
 
+
+    public Author() {
+    }
+
     public Author(String firstname, String lastname ) {
         this.firstname = firstname;
         this.lastname = lastname;
